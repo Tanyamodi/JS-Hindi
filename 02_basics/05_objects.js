9cf000ea9deee704401a9c5656152565cfb34119
Tanya Modi <111244982+Tanyamodi@users.noreply.github.com>
2024-02-14 07:32:52 +0000

    json talk

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,35 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+
+// {
+//     "name": "tanya",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+//in react
+// const navbar = ({company}) => {
+
+
+// }
+
+// navbar(comapny = "hitesh")
+
+
